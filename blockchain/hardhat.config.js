6de023a5ab12c21adc9c772090b0879a90790ae2
Orcun <orcun@mindbehind.com>
2022-06-01 02:40:15 +0300

    complete backend and add fron end

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-
+require("dotenv").config({ path: ".env" });
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -16,6 +16,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.4",
+ module.exports = {
+  solidity: '0.8.1',
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/0YJNZN1FusCXfCjzdAxPp1oRV60NDN3J",
+      accounts: ["4fa6c2e75da274e014ff66e8e8ea1df755b443283d95d07673eb680c1ea8f32e"]
+    },
+  },
 };
---
+++ client/README.md
@@ -0,0 +1,6 @@
+Welcome 👋
+To get started with this course, clone this repo and follow these commands:
+
+Run npm install at the root of your directory
+Run npm run start to start the project
+Start coding!
